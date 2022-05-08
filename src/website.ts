08d3e2e3ad5057fabc997fd84b0aa6f2a62daa1f
KadeS04 <kadestiles@yahoo.com>
2022-05-07 20:12:56 -0500

    1 file modified

--- src/website.ts
+++ src/website.ts
@@ -1,5 +1,12 @@
-export interface Website {
-  rating: number;
+export interface Dinner {
   name: string;
-  
+  rating: number;
+  description: string;
+}
+
+export interface Movie {
+  name: string;
+  rating: number;
+  length: number;
+  description: string;
 }
