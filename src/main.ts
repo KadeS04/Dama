93f4e0dfd8aa4ad2b3ad9de9b2655037be2f29c0
KadeS04 <kadestiles@yahoo.com>
2022-05-07 20:09:57 -0500

    2 files modified, 1 added

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
-
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
---
+++ src/website.ts
@@ -0,0 +1,5 @@
+export interface Website {
+  rating: number;
+  name: string;
+  
+}
